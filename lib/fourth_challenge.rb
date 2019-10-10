2c49749004601b1412df117f070f350aae41fdb5
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 15:31:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,7 @@ def fourth_challenge
           :attitude => "worried"
         },
         {
-          :name => "mercutio",
+          :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
         }
