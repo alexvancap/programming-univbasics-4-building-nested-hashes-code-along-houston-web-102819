cdc671fa7aed99841796a56fa873042c7c2a1f78
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 15:13:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,21 +1,20 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
-      :hero_friends => [
-        {
+  :montague => {
+    :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
          :name => "Benvolio",
          :age => "17",
          :attitude => "worried"
@@ -25,24 +24,24 @@ def third_challenge
         :age => "18",
         :attitude => "hot-headed"
       }
-      ]
-   },
-   :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-        },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-        },
-      :heroine => {
-        :name => "juliet",
-        :age => "15",
-        :status => "alive"
-        },
-      :heroine_friends => [
-        {
+    ]
+  },
+  :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
         :name => "Steven",
         :age => "30",
         :attitude => "confused"
@@ -52,9 +51,7 @@ def third_challenge
         :age => "44",
         :attitude => "worried"
       }
-      ]
-   }
+    ]
   }
-
-
+}
 end
