58c759b47036dc67834f2537b87ddbac90c62b02
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 15:30:21 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -28,7 +28,9 @@ def fourth_challenge
           :attitude => "confused"
         },
         {
-
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
         }
       ]
    }
